f7312b74ba5b8a80e232362d2291c36a5ce29427
jjherrerarodriguez98 <JUAN.HERRERARODRIGUEZ@ucr.ac.cr>
2020-12-03 10:56:55 -0600

    me cago en la puta

--- lamilputa2/src/common/cola/Cola.cc
+++ lamilputa2/src/common/cola/Cola.cc
@@ -103,4 +103,4 @@ template class Cola<struct Broadcast>;
 template class Cola<struct CapaEnlace>;
 template class Cola<struct ArbolGenerador>;
 template class Cola<struct DatosArbolGenerador>;
-template class Cola<struct Latido>
+template class Cola<struct Latido>;
